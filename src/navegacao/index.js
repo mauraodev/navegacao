912cae3c2c245356356c9cc09589a1cfeb5ed1fd
Mauro Nunes <maurao.dev@gmail.com>
2021-07-26 12:19:44 -0300

    Adicionando ícones no projeto

--- src/navegacao/index.js
+++ src/navegacao/index.js
@@ -4,15 +4,15 @@ import { SafeAreaView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
 // import Stack from './Stack';
-// import Tab from './Tab';
-import Drawer from './Drawer';
+import Tab from './Tab';
+// import Drawer from './Drawer';
 
 export default () => (
   <SafeAreaView style={{ flex: 1 }}>
     <NavigationContainer>
       {/* <Stack /> */}
-      {/* <Tab /> */}
-      <Drawer />
+      <Tab />
+      {/* <Drawer /> */}
     </NavigationContainer>
   </SafeAreaView>
 );
